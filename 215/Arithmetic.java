c295635332522ef4c07638c33c890ad69f1c204b
kpsketch <parneet.kaur533@triosstudent.com>
2025-07-18 00:16:45 -0400

    updated 215

--- 215/Arithmetic.java
+++ 215/Arithmetic.java
@@ -3,9 +3,9 @@ public class Arithmetic {
   public static void main (String[] args)
   {
   Scanner input = new Scanner(System.in);
-  System.out.print(Enter first integer");
+  System.out.print("Enter first integer");
   int x = input.nextInt();// read first inetger
-  System.out.print("Enter second integer");
+  System.out.print("Enter second interger");
   int y = input.nextInt();
  int sum = x + y;
         int product = x * y;
@@ -13,9 +13,10 @@ public class Arithmetic {
         int division = x / y;
 
          System.out.printf("Sum is %d%n" , sum);
-            System.out.printf"Product is %d%n " , product);
+            System.out.printf("Product is %d%n " , product);
             System.out.printf("Difference is %d%n " , difference);
-            System.out.printf("Quotient is %d%n" , division");
-}
+            System.out.printf("Quotient is %d%n" , division);
+
 input.close();
+  }
 }
